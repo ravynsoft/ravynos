5583f5439586e6330f3c161f7d54e3561575d193
pkgdemon <pkgdemonteam@gmail.com>
2019-11-28 09:47:08 -0500

    Add script to build FuryBSD iso from ports tag

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This script will build FuryBSD iso from latest github tag for furybsd-wallpapers
+
+# Only run as superuser
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+gitcheck=$(git ls-remote --tags https://github.com/furybsd/furybsd-wallpapers | awk '{ print $2}' | cut -d'/' -f3 | head -n 1)
+
+rm /usr/local/furybsd/iso/FuryBSD-12.0-XFCE-${gitcheck}.iso || true
+cd ${cwd} && ./build.sh xfce ${gitcheck}
+md5 /usr/local/furybsd/iso/FuryBSD-12.0-XFCE-${gitcheck}.iso > /usr/local/furybsd/iso/FuryBSD-12.0-XFCE-${gitcheck}.iso.md5
+
