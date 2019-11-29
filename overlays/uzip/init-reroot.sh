00903600dc4b32404fa054403d0c4e3e0fa5c8dd
pkgdemon <pkgdemonteam@gmail.com>
2019-11-28 21:59:35 -0500

    Fix init-reroot which has to run twice for some reason and was being too chatty

--- overlays/uzip/init-reroot.sh
+++ overlays/uzip/init-reroot.sh
@@ -2,35 +2,32 @@
 
 PATH="/rescue"
 
-mount -u -w /
-mkdir -p /cdrom /union /usr
-mount_cd9660 /dev/iso9660/FURYBSD /cdrom
-mdmfs -P -F /cdrom/data/usr.uzip -o ro md.uzip /usr
-mdmfs -s 512m md /union || exit 1 
-mount -t unionfs /union /usr
+mount -u -w / >/dev/null 2>/dev/null
+mkdir -p /cdrom /union /usr >/dev/null 2>/dev/null
+mount_cd9660 /dev/iso9660/FURYBSD /cdrom >/dev/null 2>/dev/null
+mdmfs -P -F /cdrom/data/usr.uzip -o ro md.uzip /usr >/dev/null 2>/dev/null
+mdmfs -s 512m md /union >/dev/null 2>/dev/null
+mount -t unionfs /union /usr >/dev/null 2>/dev/null
 
 BOOTMODE=`sysctl -n machdep.bootmethod`
 export BOOTMODE
 
 if [ "${BOOTMODE}" = "BIOS" ]; then
-  echo "BIOS detected"
-  cp /usr/home/liveuser/xorg.conf.d/driver-vesa.conf /etc/X11/xorg.conf
+  cp /usr/home/liveuser/xorg.conf.d/driver-vesa.conf /etc/X11/xorg.conf >/dev/null 2>/dev/null
 fi
 
 if [ "${BOOTMODE}" = "UEFI" ]; then
-  echo "UEFI detected"
-  cp /usr/home/liveuser/xorg.conf.d/driver-scfb.conf /etc/X11/xorg.conf
+  cp /usr/home/liveuser/xorg.conf.d/driver-scfb.conf /etc/X11/xorg.conf >/dev/null 2>/dev/null
 fi
 
 VMGUEST=`sysctl -n kern.vm_guest`
 export VMGUEST
 
 if [ "${VMGUEST}" = "xen" ]; then
-  echo "XEN guest detected"
-  sysrc devd_enable="NO"
+  /usr/sbin/sysrc devd_enable="NO" >/dev/null 2>/dev/null
 fi
 
-/usr/sbin/sysrc -f /etc/rc.conf kld_list+="sysctlinfo"
+/usr/sbin/sysrc -f /etc/rc.conf kld_list+="sysctlinfo" >/dev/null 2>/dev/null
 
-kenv init_shell="/bin/sh"
+kenv init_shell="/bin/sh" >/dev/null 2>/dev/null
 exit 0
