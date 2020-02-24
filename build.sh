3400e5f8f8c2801bf22ebb360214b29b89114b1e
pkgdemon <pkgdemonteam@gmail.com>
2020-02-23 19:11:58 -0500

    Begin build system conversion to ZFS/poudriere * Create jail or update if it already exists. * Add functions for ports, build, and image to be worked on next.

--- build.sh
+++ build.sh
@@ -18,11 +18,6 @@ label="FURYBSD"
 isopath="${iso}/${vol}.iso"
 desktop=$1
 tag=$2
-export DISTRIBUTIONS="kernel.txz base.txz"
-export BSDINSTALL_DISTSITE="http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/12.0-RELEASE/"
-export BSDINSTALL_CHROOT="/usr/local/furybsd/uzip"
-export BSDINSTALL_DISTDIR="/usr/local/furybsd/cache/12.0/base"
-
 
 # Only run as superuser
 if [ "$(id -u)" != "0" ]; then
@@ -83,187 +78,53 @@ isopath="${iso}/${vol}.iso"
 
 workspace()
 {
-  umount ${uzip}/var/cache/pkg >/dev/null 2>/dev/null
-  umount ${ports} >/dev/null 2>/dev/null
-  rm -rf ${ports} >/dev/null 2>/dev/null
-  umount ${cache}/furybsd-packages/
-  rm ${cache}/master.zip >/dev/null 2>/dev/null
-  umount ${uzip}/dev >/dev/null 2>/dev/null
-  if [ -d "${livecd}" ] ;then
-    chflags -R noschg ${uzip} ${cdroot} >/dev/null 2>/dev/null
-    rm -rf ${uzip} ${cdroot} ${ports} >/dev/null 2>/dev/null
+  # Make sure directory exists to install poudriere config
+  if [ ! -d "${livecd}" ] ; then
+    mkdir -p ${livecd}
   fi
-  mkdir -p ${livecd} ${base} ${iso} ${packages} ${uzip} ${ramdisk_root}/dev ${ramdisk_root}/etc >/dev/null 2>/dev/null
+  # Install poudriere config
+  if [ ! -f "${livecd}/poudriere.conf" ] ; then
+    cp ${cwd}/poudriere.conf ${livecd}/poudriere.conf
+  fi
 }
 
-base()
+jail()
 {
-  if [ ! -f "${base}/base.txz" ] ; then 
-    bsdinstall distfetch
+  # Check if jail exists
+  poudriere -e ${livecd} jail -l | grep -q furybsd
+  if [ $? -eq 1 ] ; then
+    # If jail does not exist create it
+    poudriere -e ${livecd} jail -c -j furybsd -v ${version}-RELEASE -K GENERIC
+  else
+    # Update jail if it exists
+    poudriere -e ${livecd} jail -u -j furybsd
   fi
-  
-  if [ ! -f "${base}/kernel.txz" ] ; then
-    cd ${base}
-    bsdinstall distfetch
-  fi
-  bsdinstall distextract
-}
-
-packages()
-{
-  cp /etc/resolv.conf ${uzip}/etc/resolv.conf
-  mkdir ${uzip}/var/cache/pkg
-  mount_nullfs ${packages} ${uzip}/var/cache/pkg
-  mount -t devfs devfs ${uzip}/dev
-  cat ${cwd}/settings/packages.common | xargs pkg-static -c ${uzip} install -y
-  cat ${cwd}/settings/packages.${desktop} | xargs pkg-static -c ${uzip} install -y
-  rm ${uzip}/etc/resolv.conf
-  umount ${uzip}/var/cache/pkg
-  umount ${uzip}/dev
 }
 
 ports()
 {
-  cp /etc/resolv.conf ${uzip}/etc/resolv.conf
-  cd ${cache} && fetch https://github.com/furybsd/furybsd-ports/archive/master.zip
-  cd ${cache} && unzip master.zip
-  cd ${ports} && ./furybsd-make-ports
-  mkdir -p ${uzip}/usr/local/furybsd/cache/furybsd-packages
-  mkdir -p ${uzip}/usr/local/etc/pkg/repos
-  cp ${cwd}/furybsd.conf ${uzip}/usr/local/etc/pkg/repos
-  mount -t nullfs ${cache}/furybsd-packages/ ${uzip}/usr/local/furybsd/cache/furybsd-packages/
-  mount -t devfs devfs ${uzip}/dev
-  case $desktop in
-    'kde')
-      chroot ${uzip} pkg install -fy furybsd-kde-desktop
-      ;;
-    'gnome')
-      chroot ${uzip} pkg install -fy furybsd-gnome-desktop
-      ;;
-    *)
-      chroot ${uzip} pkg install -fy furybsd-xfce-desktop
-      ;;
-  esac
-  rm ${cache}/master.zip
-  rm ${uzip}/etc/resolv.conf
-  umount ${uzip}/dev
-  umount ${cache}/furybsd-packages/
-  rm -rf ${ports}
-  rm -rf ${uzip}/usr/local/furybsd/cache/furybsd-packages/
-  rm -rf ${uzip}/usr/local/etc/pkg
-}
-
-rc()
-{
-  if [ ! -f "${uzip}/etc/rc.conf" ] ; then
-    touch ${uzip}/etc/rc.conf
-  fi
-  if [ ! -f "${uzip}/etc/rc.conf.local" ] ; then
-    touch ${uzip}/etc/rc.conf.local
-  fi
-  cat ${cwd}/settings/rc.conf.common | xargs chroot ${uzip} sysrc -f /etc/rc.conf.local
-  cat ${cwd}/settings/rc.conf.${desktop} | xargs chroot ${uzip} sysrc -f /etc/rc.conf.local
-}
-
-live-settings()
-{
-  cp ${cwd}/nginx.conf ${uzip}/usr/local/etc/nginx/nginx.conf
-  cp ${uzip}/usr/local/www/phpsysinfo/phpsysinfo.ini.new ${uzip}/usr/local/www/phpsysinfo/phpsysinfo.ini
-  cp ${uzip}/usr/local/etc/php.ini-production ${uzip}/usr/local/etc/php.ini
-}
-
-user()
-{
-  mkdir -p ${uzip}/usr/home/liveuser/Desktop
-  cp ${cwd}/fury-install ${uzip}/usr/home/liveuser/
-  cp -R ${cwd}/xorg.conf.d/ ${uzip}/usr/home/liveuser/xorg.conf.d
-  cp ${cwd}/fury-config-xorg.desktop ${uzip}/usr/home/liveuser/Desktop/
-  cp ${cwd}/fury-install.desktop ${uzip}/usr/home/liveuser/Desktop/
-  cp ${cwd}/fury-sysinfo.desktop ${uzip}/usr/home/liveuser/Desktop/
-  cp ${cwd}/fury-desktop-readme.txt ${uzip}/usr/home/liveuser/Desktop/"Getting Started.txt"
-  chroot ${uzip} echo furybsd | chroot ${uzip} pw mod user root -h 0
-  chroot ${uzip} pw useradd liveuser -u 1000 \
-  -c "Live User" -d "/home/liveuser" \
-  -g wheel -G operator -m -s /bin/csh -k /usr/share/skel -w none
-  chroot ${uzip} pw groupadd liveuser -g 1000
-  chroot ${uzip} echo furybsd | chroot ${uzip} pw mod user liveuser -h 0
-  chroot ${uzip} chown -R 1000:1000 /usr/home/liveuser
-}
-
-dm()
-{
-  case $desktop in
-    'kde')
-      cp ${cwd}/sddm.conf ${uzip}/usr/local/etc/
-      ;;
-    'gnome')
-      cp ${cwd}/custom.conf ${uzip}/usr/local/etc/gdm/custom.conf
-      ;;
-    *)
-      cp ${cwd}/lightdm.conf ${uzip}/usr/local/etc/lightdm/
-      chroot ${uzip} sed -i '' -e 's/memorylocked=128M/memorylocked=256M/' /etc/login.conf
-      chroot ${uzip} cap_mkdb /etc/login.conf
-      ;;
-  esac
-}
-
-uzip() 
-{
-  cp -R ${cwd}/overlays/uzip/ ${uzip}
-  install -o root -g wheel -m 755 -d "${cdroot}"
-  makefs "${cdroot}/data/usr.ufs" "${uzip}/usr"
-  mkuzip -o "${cdroot}/data/usr.uzip" "${cdroot}/data/usr.ufs"
-  rm -f "${cdroot}/data/usr.ufs"
-  chflags -R noschg ${uzip}/usr
-  rm -rf ${uzip}/usr
-  makefs "${cdroot}/data/system.ufs" "${uzip}"
-  mkuzip -o "${cdroot}/data/system.uzip" "${cdroot}/data/system.ufs"
-  rm -f "${cdroot}/data/system.ufs"
-}
-
-ramdisk() 
-{
-  cp -R ${cwd}/overlays/ramdisk/ ${ramdisk_root}
-  cd "${uzip}" && tar -cf - rescue | tar -xf - -C "${ramdisk_root}"
-  touch "${ramdisk_root}/etc/fstab"
-  cp ${uzip}/etc/login.conf ${ramdisk_root}/etc/login.conf
-  makefs -b '10%' "${cdroot}/data/ramdisk.ufs" "${ramdisk_root}"
-  gzip "${cdroot}/data/ramdisk.ufs"
-  rm -rf "${ramdisk_root}"
-}
-
-boot() 
-{
-  cp -R ${cwd}/overlays/boot/ ${cdroot}
-  cd "${uzip}" && tar -cf - --exclude boot/kernel boot | tar -xf - -C "${cdroot}"
-  for kfile in kernel geom_uzip.ko nullfs.ko tmpfs.ko unionfs.ko; do
-  tar -cf - boot/kernel/${kfile} | tar -xf - -C "${cdroot}"
-  done
-}
-
-image() 
-{
-  sh ${cwd}/scripts/mkisoimages.sh -b $label $isopath ${cdroot}
-}
-
-cleanup()
-{
-  if [ -d "${livecd}" ] ; then
-    chflags -R noschg ${uzip} ${cdroot} >/dev/null 2>/dev/null
-    rm -rf ${uzip} ${cdroot} >/dev/null 2>/dev/null
+  # Check if ports tree exists
+  poudriere -e ${livecd} ports -l | grep -q furybsd
+  if [ $? -eq 1 ] ; then
+    # If ports tree does not exist create it
+    poudriere -e ${livecd} ports -c -p furybsd-ports -b branches/2020Q1 -m git
+  else
+    # Update ports tree if it exists
+    poudriere -e ${livecd} ports -u -p furybsd-ports -b branches/2020Q1 -m git
   fi
 }
 
-#workspace
-#base
-#packages
+build()
+{
+  poudriere -e ${livecd} bulk -j furybsd -p furybsd-ports -f ~/furybsd-pkglist
+}
+image()
+{
+  poudriere -e ${livecd} image -t iso -j furybsd -s 4g -p furybsd-ports -h furybsd -n ${vol} -f ~/furybsd-pkglist
+}
+
+workspace
+jail
+#build
 #ports
-#rc
-#dm
-#live-settings
-#user
-#uzip
-#ramdisk
-#boot
 #image
-#cleanup
