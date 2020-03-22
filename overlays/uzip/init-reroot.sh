19c1ab554b9ac7dcab27f5a7eb0a6fc6dc1f67c9
pkgdemon <pkgdemonteam@gmail.com>
2020-03-22 14:56:52 -0400

    Changes to integrate virtualbox guest additions * Remove when not applicable.

--- overlays/uzip/init-reroot.sh
+++ overlays/uzip/init-reroot.sh
@@ -9,7 +9,7 @@ mdmfs -P -F /cdrom/data/usr.uzip -o ro md.uzip /usr >/dev/null 2>/dev/null
 mdmfs -s 512m md /union >/dev/null 2>/dev/null
 mount -t unionfs /union /usr >/dev/null 2>/dev/null
 
-BOOTMODE=`sysctl -n machdep.bootmethod`
+BOOTMODE=$(sysctl -n machdep.bootmethod)
 export BOOTMODE
 
 if [ "${BOOTMODE}" = "BIOS" ]; then
@@ -20,7 +20,7 @@ if [ "${BOOTMODE}" = "UEFI" ]; then
   cp /usr/home/liveuser/xorg.conf.d/driver-scfb.conf /etc/X11/xorg.conf >/dev/null 2>/dev/null
 fi
 
-VMGUEST=`sysctl -n kern.vm_guest`
+VMGUEST=$(sysctl -n kern.vm_guest)
 export VMGUEST
 
 if [ "${VMGUEST}" = "xen" ]; then
@@ -34,6 +34,19 @@ if [ "${VMGUEST}" = "vmware" ]; then
   /usr/sbin/sysrc -f /etc/rc.conf vmware_guest_vmmemctl_enable="YES" >/dev/null 2>/dev/null
   /usr/sbin/sysrc -f /etc/rc.conf vmware_guestd_enable="YES" >/dev/null 2>/dev/null
   /usr/sbin/sysrc -f /etc/rc.conf moused_enable="YES" >/dev/null 2>/dev/null
+fi
+
+
+if [ -f "/usr/sbin/pciconf" ] ; then
+  /usr/sbin/pciconf -lv 2>/dev/null | /usr/bin/grep -q VirtualBox 2>/dev/null
+  if [ $? -eq 0 ] ; then
+    rm /etc/X11/xorg.conf >/dev/null 2>/dev/null
+    cp /usr/home/liveuser/xorg.conf.d/driver-virtualbox.conf /etc/X11/xorg.conf >/dev/null 2>/dev/null
+    /usr/sbin/sysrc -f /etc/rc.conf vboxguest_enable="YES" >/dev/null 2>/dev/null
+    /usr/sbin/sysrc -f /etc/rc.conf vboxservice_enable="YES" >/dev/null 2>/dev/null
+  else
+    /usr/sbin/pkg delete -fy virtualbox-ose-additions >/dev/null 2>/dev/null
+  fi
 fi
 
 /usr/sbin/sysrc -f /etc/rc.conf kld_list+="sysctlinfo" >/dev/null 2>/dev/null
