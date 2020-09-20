20242ccc9f25e34f183b40f101f02fe95d112ead
probonopd <probonopd@users.noreply.github.com>
2020-09-20 08:32:09 +0200

    Fix installer background image

--- overlays/uzip/furybsd-live-settings/files/usr/local/share/data/freebsd-install/freebsd-install.py
+++ overlays/uzip/furybsd-live-settings/files/usr/local/share/data/freebsd-install/freebsd-install.py
@@ -135,10 +135,8 @@ class InstallWizard(QtWidgets.QWizard, object):
         # Remove window decorations, especially the close button
         self.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        # self.setPixmap(QtWidgets.QWizard.BackgroundPixmap, os.path.dirname(__file__) + '/Background.png') # This works in PySide2
-        pxmp = QtGui.QPixmap(os.path.dirname(__file__) + '/Background.png')
-        print("pxmp.height()", pxmp.height()) # FIXME: This shows that we have the pixmap loaded
-        self.setPixmap(QtWidgets.QWizard.WatermarkPixmap, pxmp) # FIXME: Why is this not working in PyQt5?
+
+        self.setPixmap(QtWidgets.QWizard.BackgroundPixmap, QtGui.QPixmap(os.path.dirname(__file__) + '/Background.png'))
 
         self.setOption(QtWidgets.QWizard.ExtendedWatermarkPixmap, True)
         # self.setPixmap(QtWidgets.QWizard.LogoPixmap, 'Logo.png')
