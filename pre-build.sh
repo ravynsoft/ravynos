94afaf17865ce8eed879d0565a57ebe1aa4dcc20
probonopd <probonopd@users.noreply.github.com>
2020-10-11 16:14:57 +0200

    First build

---
+++ pre-build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# TODO: Replace transfer.sh by a better solution
+
+# Exit on errors
+set -e
+
+#################
+
+cd overlays/uzip/hello/files/System
+
+# Filer
+wget https://github.com/helloSystem/Filer/releases/download/continuous/Filer_FreeBSD.zip
+unzip Filer_FreeBSD.zip
+rm -f Filer_FreeBSD.zip
+
+# QtPlugin
+wget https://github.com/helloSystem/QtPlugin/releases/download/continuous/QtPlugin_FreeBSD.zip
+unzip QtPlugin_FreeBSD.zip
+cp -Rf QtPlugin/ /
+rm -f QtPlugin_FreeBSD.zip
+rm -rf QtPlugin/
+
+# Menu
+wget https://github.com/helloSystem/Menu/releases/download/continuous/Menu_FreeBSD.zip
+unzip Menu_FreeBSD.zip
+rm -f Menu_FreeBSD.zip
+
+# Dock
+wget https://github.com/helloSystem/Dock/releases/download/continuous/Dock_FreeBSD.zip
+unzip Dock_FreeBSD.zip
+rm -f Dock_FreeBSD.zip
+
+cd -
