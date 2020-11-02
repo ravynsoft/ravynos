56c8b54aff8c29ac8042ae644a682acfb9cab096
probonopd <probonopd@users.noreply.github.com>
2020-11-02 19:20:26 +0100

    Add FyneDesk repo [ci skip]

--- overlays/uzip/create-livemedia/files/usr/local/share/data/create-livemedia/create-livemedia.py
+++ overlays/uzip/create-livemedia/files/usr/local/share/data/create-livemedia/create-livemedia.py
@@ -158,7 +158,7 @@ class IntroPage(QtWidgets.QWizardPage, object):
         # Repo dropdown
 
         self.repo_menu = QtWidgets.QComboBox()
-        self.available_repos = ["https://api.github.com/repos/helloSystem/ISO/releases", "https://api.github.com/repos/probonopd/furybsd-livecd/releases", "https://api.github.com/repos/probonopd/ghostbsd-build/releases"]
+        self.available_repos = ["https://api.github.com/repos/helloSystem/ISO/releases", "https://api.github.com/repos/probonopd/furybsd-livecd/releases", "https://api.github.com/repos/probonopd/ghostbsd-build/releases", "https://api.github.com/repos/andydotxyz/furybsd-livecd/releases"]
         for available_repo in self.available_repos:
             self.repo_menu.addItem("/".join(available_repo.split("/")[4:6]))
         self.other_iso = "Other..."
