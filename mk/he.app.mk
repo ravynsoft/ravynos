190b5385d13b2736c272b98254ee1139beedec34
Zoë Knox <zoe@pixin.net>
2021-02-02 22:03:55 -0500

    [1] Add makefile include for AppDir bundle builds

---
+++ mk/he.app.mk
@@ -0,0 +1,39 @@
+APP_DIR=${APP}.app
+NO_ROOT=yes
+PROG=${APP_DIR}/Contents/Helium/${APP}
+MAN=
+
+UID != id -u
+.if defined(UID) && ${UID} != 0
+INSTALLFLAGS=-U
+.endif
+
+.if defined(FRAMEWORKS) && !empty(FRAMEWORKS)
+_LIBDIRS=
+_INCDIRS=
+_LIBS=
+.for fmwk in ${FRAMEWORKS}
+_INCDIRS+= -I${fmwk}.framework/Headers
+_FWLIBDIR= ${fmwk}.framework/Versions/Current
+_CURVER!= readlink ${_FWLIBDIR}
+_VERLIBDIR= ${_FWLIBDIR:S%Current%${_CURVER}%}
+_LIBDIRS+= -L${_VERLIBDIR}
+_LIBFILES!=echo ${_FWLIBDIR}/*.so
+.for lib in ${_LIBFILES}
+_LIBS+= -l${lib:C%.*lib%%:C%\.so$%%} -Wl,--rpath=${_VERLIBDIR}
+.endfor
+.endfor
+CFLAGS+= ${_INCDIRS}
+LDFLAGS+= ${_LIBDIRS} ${_LIBS}
+.endif
+
+all: ${APP_DIR} ${PROG}
+
+${APP_DIR}:
+	@${ECHO} building ${APP_DIR} bundle
+	mkdir -p "${APP_DIR}/Contents/Helium" \
+		"${APP_DIR}/Contents/Resources"
+	touch "${APP_DIR}/Contents/Info.plist"
+	touch "${APP_DIR}/Contents/PkgInfo"
+
+.include <bsd.prog.mk>
