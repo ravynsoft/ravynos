49b2d5320c8c1cc24874eaba74656e337b6ac10a
Zoë Knox <zoe@pixin.net>
2021-03-08 07:02:55 -0500

    Handle multiple frameworks properly

--- mk/he.app.mk
+++ mk/he.app.mk
@@ -12,19 +12,19 @@ INSTALLFLAGS=-U
 _LIBDIRS=
 _INCDIRS=
 _LIBS=
+_VERLIBDIR=
+_LIBPATTERN=echo 
 .for fmwk in ${FRAMEWORKS}
 _INCDIRS+= -I${fmwk:tW:S/^..\//${APP_DIR}\/Contents\/Helium\/..\//1}.framework/Headers
-_RELLIBDIR= ${fmwk}.framework/Versions/Current 
-_FWLIBDIR= ${fmwk:tW:S/^..\//${APP_DIR}\/Contents\/Helium\/..\//1}.framework/Versions/Current
-_CURVER!= readlink ${_FWLIBDIR}
-_VERLIBDIR= ${_RELLIBDIR:S%Current%${_CURVER}%}
-_LIBDIRS+= -L${_FWLIBDIR}
-_LIBFILES!=echo ${_FWLIBDIR}/*.so
-.for lib in ${_LIBFILES}
-_LIBS+= -l${lib:C%.*lib%%:C%\.so$%%} \
-	-Wl,--rpath=${_VERLIBDIR:tW:S/..\//\$ORIGIN\/..\//1:Q}
+_LIBDIRS+= -L${fmwk:tW:S/^..\//${APP_DIR}\/Contents\/Helium\/..\//1}.framework/Versions/Current
+_LIBPATTERN+= ${fmwk:tW:S/^..\//${APP_DIR}\/Contents\/Helium\/..\//1}.framework/Versions/Current/*.so  
+_VERLIBDIR+= -Wl,--rpath=${fmwk:tW:S/$/.frameworks\/Versions\/${fmwk:tW:S/$/.framework\/Versions\/Current/:tA:T}/:S/..\//\$ORIGIN\/..\//1:Q}
 .endfor
+
+.for lib in ${_LIBPATTERN:sh}
+_LIBS+= -l${lib:C%.*lib%%:C%\.so$%%}
 .endfor
+
 CFLAGS+= ${_INCDIRS}
 LDFLAGS+= ${_LIBDIRS} ${_LIBS}
 .endif
