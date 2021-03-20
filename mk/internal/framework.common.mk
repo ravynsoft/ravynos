eb491480aa48caf71ab8c19c37fddea874361431
Zoë Knox <zoe@pixin.net>
2021-03-20 15:41:53 -0400

    Include RUNPATH and NEEDED entries to satisfy transitive deps

--- mk/internal/framework.common.mk
+++ mk/internal/framework.common.mk
@@ -2,5 +2,5 @@
 FMWK_CFLAGS := -O0 -g -D__HELIUM__ -DBSD -DPLATFORM_IS_POSIX -DGCC_RUNTIME_3 \
 	 -DPLATFORM_USES_BSD_SOCKETS -fobjc-runtime=gnustep-2.0 \
 	 -fobjc-nonfragile-abi -fPIC
-FMWK_LDFLAGS+= -L${BUILDROOT}/usr/lib -L/usr/lib -lobjc -lunwind -lpthread
+FMWK_LDFLAGS+= -L${BUILDROOT}/usr/lib -L/usr/lib -Wl,--no-as-needed
 
