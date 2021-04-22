103d18186e31766d06f0d6a24780e574d8378a63
probonopd <probonopd@users.noreply.github.com>
2021-04-22 20:21:29 +0200

    Nvidia is now 460?

--- build.sh
+++ build.sh
@@ -299,9 +299,9 @@ initgfx()
     for ver in '' 390 340 304; do
         pkgfile=$(/usr/local/sbin/pkg-static -c ${uzip} rquery %n-%v.txz nvidia-driver${ver:+-$ver})
         fetch -o "${cache}/" "https://pkg.freebsd.org/FreeBSD:${MAJOR}:amd64/${PKGS}/All/${pkgfile}"
-        mkdir -p "${uzip}/usr/local/nvidia/${ver:-440}/"
-        tar xfC "${cache}"/${pkgfile} "${uzip}/usr/local/nvidia/${ver:-440}/"
-        ls "${uzip}/usr/local/nvidia/${ver:-440}/+COMPACT_MANIFEST"
+        mkdir -p "${uzip}/usr/local/nvidia/${ver:-460}/"
+        tar xfC "${cache}"/${pkgfile} "${uzip}/usr/local/nvidia/${ver:-460}/"
+        ls "${uzip}/usr/local/nvidia/${ver:-460}/+COMPACT_MANIFEST"
     done
   fi
 
