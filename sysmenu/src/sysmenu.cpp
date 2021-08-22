c8137b48e0c0a5006cd0e709943bbe6dc0500c61
Zoe Knox <zoe@pixin.net>
2021-08-22 08:59:48 -0400

    Experimental linkage to ObjC++ in plasmoid

--- sysmenu/src/sysmenu.cpp
+++ sysmenu/src/sysmenu.cpp
@@ -5,6 +5,8 @@
 #include "version.h"
 #include <QDebug>
 
+extern "C" const char * getCpuInfo();
+
 AXMessageBox::AXMessageBox()
     : QMessageBox()
 {
@@ -38,14 +40,14 @@ void AiryxMenu::aboutThisComputer()
     m_about->setWindowTitle("About this Computer");
     m_about->setStandardButtons(0);
     m_about->setText("<table borders=\"0\"><tr><td align=\"center\" valign=\"middle\">"
-                   "<img src=\"/usr/share/plasma/plasmoids/org.airyx.plasma.AiryxMenu/contents/images/tanuki_logo.png\">"
-                   "</td><td>&nbsp;&nbsp;</td><td>"
+                   "<img width=\"192\" height=\"192\" src=\"/usr/share/plasma/plasmoids/org.airyx.plasma.AiryxMenu/contents/images/tanuki_logo.png\">"
+                   "</td><td>&nbsp;&nbsp;</td><td width=\"100%\">"
                    "<font face=\"Nimbus Sans\"><font size=\"+8\"><b>airyxOS</b> " AIRYX_CODENAME "</font><br>"
                    "Version " AIRYX_VERSION "<br>&nbsp;<br>&nbsp;<br>"
-                   "<b>Model from BIOS</b><br>"
-                   "<b>Processor</b> xxx xxx xxx<br>"
-                   "<b>Memory</b> xxxx xxxx xx<br>"
-                   "<b>Graphics</b> xxxxx xxxx xxx<br>"
+                   "<b>Model from BIOS</b><br><br>"
+                   "<b>Processor</b> "+ QString(getCpuInfo()) +"<br><br>"
+                   "<b>Memory</b> xxxx xxxx xx<br><br>"
+                   "<b>Graphics</b> xxxxx xxxx xxx<br><br>"
                    "<br>"
                    "</font></td></tr></table>");
     m_about->setWindowModality(Qt::NonModal);
