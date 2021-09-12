49eb36167f0d76aadb35b2cfb3638a78cde5b00b
Zoe Knox <zoe@pixin.net>
2021-09-11 20:55:08 -0400

    Cocoa wrapper

--- Applications/Terminal/Terminal.mm
+++ Applications/Terminal/Terminal.mm
@@ -1,4 +1,14 @@
-int main(int argc, char **argv)
+#import <Foundation/Foundation.h>
+#include <unistd.h>
+
+int main(int argc, const char **argv)
 {
-    return 0;
+    __NSInitializeProcess(argc, argv);
+    @autoreleasepool {
+        NSString *execPath = [[[NSBundle mainBundle] executablePath] autorelease];
+        execPath = [[[execPath stringByDeletingLastPathComponent]
+            stringByAppendingPathComponent:@"qterminal"] autorelease];
+        execv([execPath UTF8String], (char * const *)argv);
+    }
+    return 1;
 }
