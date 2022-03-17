ceba35849b1976e6e6dd5c69f3f982a3bbd6e9c4
Zoe Knox <zoe@pixin.net>
2022-03-17 10:38:05 -0400

    Fix include on last commit; reduce warnings on AppKit

--- Frameworks/framework.common.mk
+++ Frameworks/framework.common.mk
@@ -2,6 +2,6 @@
 FMWK_CFLAGS := -O0 -g -D__AIRYX__ -DPLATFORM_IS_POSIX -DGCC_RUNTIME_3 \
 	 -DPLATFORM_USES_BSD_SOCKETS -I/usr/include/freetype2 \
 	 -I/usr/include/fontconfig -I/usr/include/cairo -fobjc-runtime=gnustep-2.0 \
-	 -fobjc-nonfragile-abi -fPIC
+	 -fobjc-nonfragile-abi -fPIC -Wno-missing-method-return-type -Wno-macro-redefined
 FMWK_LDFLAGS+= -L${BUILDROOT}/usr/lib -Wl,--no-as-needed
 
