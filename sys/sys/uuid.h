923db6435021a58e2e32c3fa72358f10591f638c
Zoë Knox <zoe@pixin.net>
2022-04-20 12:36:52 -0400

    More merge cleanup

--- sys/sys/uuid.h
+++ sys/sys/uuid.h
@@ -89,7 +89,11 @@ void le_uuid_enc(void *buf, struct uuid const *uuid);
 
 #else	/* _KERNEL */
 
+/* Avoid conflict with Darwin uuid_t */
+#ifndef _UUID_T_DEFINED
+#define _UUID_T_DEFINED
 typedef struct uuid uuid_t;
+#endif /* _UUID_T_DEFINED */
 
 __BEGIN_DECLS
 int	uuidgen(struct uuid *, int);
