48a68ddaa60ceef0cf301b7e2b9d4ae52382ca86
Zoe Knox <zoe@pixin.net>
2022-05-20 17:31:05 -0400

    No longer need to expose vector_base_common template

--- gnu/lib/libobjc2/arc.mm
+++ gnu/lib/libobjc2/arc.mm
@@ -111,15 +111,15 @@ static inline T* new_zeroed()
 }
 
 #ifdef _LIBCPP_BEGIN_NAMESPACE_STD
-_LIBCPP_BEGIN_NAMESPACE_STD
+//_LIBCPP_BEGIN_NAMESPACE_STD
 /**
  * If we're compiling with libc++, we need to instantiate the vector base class
  * to avoid linker errors.  Libc++ defines the functions that throw exceptions
  * in the headers but makes them extern templates to avoid code duplication in
  * the slow paths.
  */
-template class __attribute__((visibility("hidden")))  __vector_base_common<true>;
-_LIBCPP_END_NAMESPACE_STD
+//template class __attribute__((visibility("hidden")))  __vector_base_common<true>;
+//_LIBCPP_END_NAMESPACE_STD
 #endif
 
 
