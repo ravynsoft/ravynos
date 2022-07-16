ce9a4997db3e99755644957816424f58cecd441a
Zoe Knox <zoe@pixin.net>
2022-07-16 16:50:34 -0400

    Dock: handle 'persistent-apps' key and tile-data

---
+++ CoreServices/Dock/DockTileData.m
@@ -0,0 +1,38 @@
+/*
+ * ravynOS Application Launcher & Status Bar
+ *
+ * Copyright (C) 2021-2022 Zoe Knox <zoe@pixin.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#import "DockTileData.h"
+
+NSDictionary *dockTileData(NSString *path) {
+    return [[NSDictionary alloc] initWithObjects:@[
+        [NSDictionary dictionaryWithObjects:@[
+            [NSDictionary dictionaryWithObjects:@[path,[NSNumber numberWithInt:0]]
+                forKeys:@[@"_CFURLString", @"_CFURLStringType"]]
+        ] forKeys:@[@"file-data"]]] forKeys:@[@"tile-data"]];
+}
+
+NSString *CFURLString(NSDictionary *dict) {
+    return [[[dict objectForKey:@"tile-data"] objectForKey:@"file-data"] objectForKey:@"_CFURLString"];
+}
+
