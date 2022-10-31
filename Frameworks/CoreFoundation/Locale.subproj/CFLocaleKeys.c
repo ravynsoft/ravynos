14566d6606644d0c099f327798750c1cbbb93886
Eleanor Dicharry <deleanor440@gmail.com>
2022-10-30 23:51:58 -0400

    Fix undefined symbol at runtime.

--- Frameworks/CoreFoundation/Locale.subproj/CFLocaleKeys.c
+++ Frameworks/CoreFoundation/Locale.subproj/CFLocaleKeys.c
@@ -132,7 +132,7 @@ CONST_STRING_DECL(kCFCalendarIdentifierEthiopicAmeteMihret, "ethiopic");
 CONST_STRING_DECL(kCFCalendarIdentifierEthiopicAmeteAlem, "ethiopic-amete-alem");
 
 // Aliases for other platforms.
-#if TARGET_OS_LINUX || TARGET_OS_BSD || TARGET_OS_WIN32 || TARGET_OS_WASI
+#if TARGET_OS_LINUX || TARGET_OS_BSD || TARGET_OS_WIN32 || TARGET_OS_WASI || __RAVYNOS__
 
 CF_EXPORT CFStringRef const kCFBuddhistCalendar __attribute__((alias ("kCFCalendarIdentifierBuddhist")));
 CF_EXPORT CFStringRef const kCFChineseCalendar __attribute__((alias ("kCFCalendarIdentifierChinese")));
