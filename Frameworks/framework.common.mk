c2bc3c1bae8b3a739e291a6342e1951f2548ec81
Zoe Knox <zoe@ravynsoft.com>
2024-04-21 10:05:33 +0000

    Simplify framework Makefiles

--- Frameworks/framework.common.mk
+++ Frameworks/framework.common.mk
@@ -1,9 +1,9 @@
 # Common settings for building frameworks
 FMWK_CFLAGS := --sysroot=${OBJTOP}/tmp -B${OBJTOP}/tmp/usr/bin \
 	 -O0 -g -D__RAVYNOS__ -DPLATFORM_IS_POSIX -DGCC_RUNTIME_3 \
-	 -DPLATFORM_USES_BSD_SOCKETS -I/usr/include/freetype2 \
-	 -I/usr/include/fontconfig -fobjc-runtime=gnustep-2.0 \
-	 -fobjc-nonfragile-abi -fPIC -Wno-missing-method-return-type -Wno-macro-redefined
+	 -DPLATFORM_USES_BSD_SOCKETS -I${SRCTOP}/Frameworks \
+	 -fobjc-runtime=gnustep-2.0 -fobjc-nonfragile-abi -fPIC \
+	 -Wno-missing-method-return-type -Wno-macro-redefined
 FMWK_LDFLAGS+= -L${BUILDROOT}/usr/lib -Wl,--no-as-needed
 
 MK_AUTO_OBJ=    yes
@@ -19,6 +19,17 @@ make-obj-dirs:
 	mkdir -p ${OBJTOP}/Frameworks/${FRAMEWORK}/${subdir}
 .endfor
 .endif
+
+std_install_hook:
+	mv ${MAKEOBJDIR}/lib${FRAMEWORK}.so \
+		${MAKEOBJDIR}/${FRAMEWORK}.framework/Versions/Current/
+	rm -rf ${BUILDROOT}/System/Library/Frameworks/${FRAMEWORK}.framework
+	cp -av ${MAKEOBJDIR}/${FRAMEWORK}.framework ${BUILDROOT}/System/Library/Frameworks/
+
+alt_install_hook:
+	rm -rf ${BUILDROOT}/System/Library/Frameworks/${FRAMEWORK}.framework
+	cp -av ${MAKEOBJDIR}/${FRAMEWORK}.framework ${BUILDROOT}/System/Library/Frameworks/
+
 
 .if !target(_libinstall)
 _libinstall: .PHONY
