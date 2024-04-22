ff35efffca91c44b482496692237a17ca6ed9b92
Zoe Knox <zoe@ravynsoft.com>
2024-04-22 05:07:37 +0000

    WIP on system build target

--- Frameworks/framework.common.mk
+++ Frameworks/framework.common.mk
@@ -3,7 +3,8 @@ FMWK_CFLAGS := --sysroot=${OBJTOP}/tmp -B${OBJTOP}/tmp/usr/bin \
 	 -O0 -g -D__RAVYNOS__ -DPLATFORM_IS_POSIX -DGCC_RUNTIME_3 \
 	 -DPLATFORM_USES_BSD_SOCKETS -I${SRCTOP}/Frameworks \
 	 -fobjc-runtime=gnustep-2.0 -fobjc-nonfragile-abi -fPIC \
-	 -Wno-missing-method-return-type -Wno-macro-redefined
+	 -Wno-missing-method-return-type -Wno-macro-redefined \
+	 -I${SRCTOP}/sys -I${SRCTOP}/include
 FMWK_LDFLAGS+= -L${BUILDROOT}/usr/lib -Wl,--no-as-needed
 
 MK_AUTO_OBJ=    yes
