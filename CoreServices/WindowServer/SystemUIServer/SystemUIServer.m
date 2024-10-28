c59a63f145de4a6a013d8c1dedd315afe98aec16
Zoe Knox <zoe@pixin.net>
2024-10-28 09:16:41 -0400

    Remove kickSession - no longer needed

--- CoreServices/WindowServer/SystemUIServer/SystemUIServer.m
+++ CoreServices/WindowServer/SystemUIServer/SystemUIServer.m
@@ -123,16 +123,6 @@ int main(int argc, const char *argv[]) {
     pthread_t kqThread;
     pthread_create(&kqThread, NULL, kqSvcLoop, (__bridge void *)del);
 
-#if 0
-    // kick off a per-user launchd to invoke LaunchAgents and per-user LaunchDaemons
-    // this starts Filer and Dock to establish the desktop session
-    NSString *kickerPath = [[NSBundle mainBundle] pathForResource:@"kickSession" ofType:@""];
-    if(kickerPath) {
-        NSLog(@"kicking off session");
-        system([kickerPath UTF8String]);
-    }
-#endif
-
     [pool drain];
     [NSApp setDelegate:del];
     [NSApp run];
