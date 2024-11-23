517e74f4014248d49b1337e8121f36190cc45c00
Zoe Knox <zoe@pixin.net>
2024-11-23 10:51:27 -0500

    Restore original VT and mode on WindowServer crash

--- CoreServices/WindowServer/main.m
+++ CoreServices/WindowServer/main.m
@@ -87,11 +87,6 @@ int main(int argc, const char *argv[]) {
     signal(SIGTHR, SIG_IGN);
     signal(SIGLIBRT, SIG_IGN);
 
-    if(setjmp(jb) != 0)
-        goto __finish; // sighandler must have caught something - get out
-
-    signal(SIGSEGV, crashHandler);
-
     /* Drop our controlling terminal - we're gonna switch */
     /* This is the recommended but sucky way. Using TIOCNOTTY isn't working */
     pid_t pid = fork();
@@ -129,6 +124,17 @@ int main(int argc, const char *argv[]) {
         FINISH(1);
     }
 
+    vtmode_t mode = {
+        .mode = VT_PROCESS,
+        .frsig = SIGUSR1,
+        .acqsig = SIGUSR1,
+        .relsig = SIGUSR2
+    };
+
+    if(setjmp(jb) != 0)
+        goto __finish; // sighandler must have caught something - get out
+    signal(SIGSEGV, crashHandler);
+
     if(ioctl(wsfd, VT_ACTIVATE, vt) < 0) {
         NSLog(@"Cannot activate terminal: %s", strerror(errno));
         FINISH(1);
@@ -138,12 +144,6 @@ int main(int argc, const char *argv[]) {
     if(tcsetsid(wsfd, getpid())  < 0)
         NSLog(@"tcsetsid: %s", strerror(errno));
 
-    vtmode_t mode = {
-        .mode = VT_PROCESS,
-        .frsig = SIGUSR1,
-        .acqsig = SIGUSR1,
-        .relsig = SIGUSR2
-    };
     if(ioctl(wsfd, VT_SETMODE, &mode) < 0)
         NSLog(@"Cannot lock VT switching: %s", strerror(errno));
 
