8980e3865f5ed75b2435267c6ec0787c94fbc143
Zoe Knox <zoe@pixin.net>
2024-11-23 17:41:44 -0500

    Start on PreferencePanes.framework [ci skip]

---
+++ Frameworks/PreferencePanes/NSPreferencePane.m
@@ -0,0 +1,78 @@
+/*
+ * ravynOS Preference Panes
+ *
+ * Copyright (C) 2024 Zoe Knox <zoe@pixin.net>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#import "NSPreferencePane.h"
+
+NSString * const NSPreferencePrefPaneIsAvailableNotification = @"NSPreferencePrefPaneIsAvailableNotification";
+NSString * const NSPreferencePaneDoUnselectNotification = @"NSPreferencePaneDoUnselectNotification";
+NSString * const NSPreferencePaneCancelUnselectNotification = @"NSPreferencePaneCancelUnselectNotification";
+NSString * const NSPreferencePaneSwitchToPaneNotification = @"NSPreferencePaneSwitchToPaneNotification";
+NSString * const NSPreferencePaneUpdateHelpMenuNotification = @"NSPreferencePaneUpdateHelpMenuNotification";
+
+NSString * const NSPrefPaneHelpMenuInfoPListKey = @"NSPrefPaneHelpAnchors";
+NSString * const NSPrefPaneHelpMenuTitleKey = @"title";
+NSString * const NSPrefPaneHelpMenuAnchorKey = @"anchor";
+
+#define kNSPrefPaneHelpMenuInfoPListKey NSPrefPaneHelpMenuInfoPListKey
+#define kNSPrefPaneHelpMenuTitleKey NSPrefPaneHelpMenuTitleKey
+#define kNSPrefPaneHelpMenuAnchorKey NSPrefPaneHelpMenuAnchorKey
+
+@implementation NSPreferencePane
+
+-(id)initWithBundle:(NSBundle *)bundle {
+    self = [super init];
+    _bundle = bundle;
+    return self;
+}
+
+-(NSView *)loadMainView {
+}
+
+-(void)assignMainView {
+}
+
+-(void)mainViewDidLoad {
+}
+
+-(void)willSelect {
+}
+
+-(void)didSelect {
+}
+
+-(void)willUnselect {
+}
+
+-(void)didUnselect {
+}
+
+-(void)replyToShouldUnselect:(BOOL)shouldUnselect {
+}
+
+
+-(void)updateHelpMenuWithArray:(NSArray *)inArrayOfMenuItems {
+}
+
+@end
+
