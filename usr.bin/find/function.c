efaa387b84ae1698f9e4693378acabeedad30761
Warner Losh <imp@FreeBSD.org>
2025-07-29 13:17:50 -0600

    find: Put back clearing the NOSTAT bit
    In fd186cd16eaf I read the condition backwards. We want to stat all the
    time until we can implement something to do it more inteligently as
    Jiles suggested in the review.
    
    Fixes: fd186cd16eaf
    Noticed by: des
    Sponsored by: Netflix
    
    (cherry picked from commit 7fc218b30b809559e16ce4709c4c4b071056c306)

--- usr.bin/find/function.c
+++ usr.bin/find/function.c
@@ -1389,6 +1389,37 @@ f_print0(PLAN *plan __unused, FTSENT *entry)
 /* c_print0 is the same as c_print */
 
 /*
+ * -printf functions --
+ *
+ *	Always true. Causes information as specified in the
+ *	argument to be written to standard output.
+ */
+int
+f_printf(PLAN *plan, FTSENT *entry)
+{
+	do_printf(plan, entry, stdout);
+	return 1;
+}
+
+PLAN *
+c_printf(OPTION *option, char ***argvp)
+{
+	PLAN *new;
+
+	/*
+	 * XXX We could scan the format looking for stat-dependent formats, and
+	 * turn off the nostat bit for trival cases: `%p`/`%f`/`%h`.
+	 */
+	isoutput = 1;
+	ftsoptions &= ~FTS_NOSTAT;
+
+	new = palloc(option);
+	new->c_data = nextarg(option, argvp);
+
+	return (new);
+}
+
+/*
  * -prune functions --
  *
  *	Prune a portion of the hierarchy.
