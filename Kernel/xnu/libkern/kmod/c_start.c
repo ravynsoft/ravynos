33323f1fb68f437a62e98d4191905978c63ca3af
Zoë Knox <zoe@pixin.net>
2026-03-29 15:21:49 -0400

    Call C++ static constructors before _start

--- Kernel/xnu/libkern/kmod/c_start.c
+++ Kernel/xnu/libkern/kmod/c_start.c
@@ -25,23 +25,7 @@
  *
  * @APPLE_OSREFERENCE_LICENSE_HEADER_END@
  */
-/*
- *   Subtle combination of files and libraries make up the C++ runtime system for kernel modules.  We are dependant on the KernelModule kmod.make and CreateKModInfo.perl scripts to be exactly instep with both this library module and the libkmod module as well.
- *
- *   If you do any maintenance on any of the following files make sure great care is taken to keep them in Sync.
- *   KernelModule.bproj/kmod.make
- *   KernelModule.bproj/CreateKModInfo.perl
- *   KernelModule.bproj/kmodc++/pure.c
- *   KernelModule.bproj/kmodc++/cplus_start.c
- *   KernelModule.bproj/kmodc++/cplus_start.c
- *   KernelModule.bproj/kmodc/c_start.c
- *   KernelModule.bproj/kmodc/c_stop.c
- *
- *   The trick is that the linkline links all of the developers modules.  If any static constructors are used .constructors_used will be left as an undefined symbol.  This symbol is exported by the cplus_start.c routine which automatically brings in the appropriate C++ _start routine.  However the actual _start symbol is only required by the kmod_info structure that is created and initialized by the CreateKModInfo.perl script.  If no C++ was used the _start will be an undefined symbol that is finally satisfied by the c_start module in the kmod library.
- *
- *   The linkline must look like this.
- *.o -lkmodc++ kmod_info.o -lkmod
- */
+
 #include <mach/mach_types.h>
 #include <libkern/OSKextLib.h>
 
@@ -49,11 +33,91 @@
 extern kmod_start_func_t *_realmain;
 extern kmod_info_t KMOD_INFO_NAME;
 
+struct mach_header_64 {
+	uint32_t	magic;		/* mach magic number identifier */
+	cpu_type_t	cputype;	/* cpu specifier */
+	cpu_subtype_t	cpusubtype;	/* machine specifier */
+	uint32_t	filetype;	/* type of file */
+	uint32_t	ncmds;		/* number of load commands */
+	uint32_t	sizeofcmds;	/* the size of all the load commands */
+	uint32_t	flags;		/* flags */
+	uint32_t	reserved;	/* reserved */
+};
+
+struct segment_command_64 { /* for 64-bit architectures */
+	uint32_t	cmd;		/* LC_SEGMENT_64 */
+	uint32_t	cmdsize;	/* includes sizeof section_64 structs */
+	char		segname[16];	/* segment name */
+	uint64_t	vmaddr;		/* memory address of this segment */
+	uint64_t	vmsize;		/* memory size of this segment */
+	uint64_t	fileoff;	/* file offset of this segment */
+	uint64_t	filesize;	/* amount to map from the file */
+	vm_prot_t	maxprot;	/* maximum VM protection */
+	vm_prot_t	initprot;	/* initial VM protection */
+	uint32_t	nsects;		/* number of sections in segment */
+	uint32_t	flags;		/* flags */
+};
+
+struct section_64 { /* for 64-bit architectures */
+	char		sectname[16];	/* name of this section */
+	char		segname[16];	/* segment this section goes in */
+	uint64_t	addr;		/* memory address of this section */
+	uint64_t	size;		/* size in bytes of this section */
+	uint32_t	offset;		/* file offset of this section */
+	uint32_t	align;		/* section alignment (power of 2) */
+	uint32_t	reloff;		/* file offset of relocation entries */
+	uint32_t	nreloc;		/* number of relocation entries */
+	uint32_t	flags;		/* flags (section type and attributes)*/
+	uint32_t	reserved1;	/* reserved (for offset or index) */
+	uint32_t	reserved2;	/* reserved (for count or sizeof) */
+	uint32_t	reserved3;	/* reserved */
+};
+
+#define LC_SEGMENT_64 0x19
+typedef void (*structor_t)(void);
+extern void kprintf(const char *fmt, ...);
+
+static void
+__call_constructors(kmod_info_t *ki)
+{
+    struct mach_header_64 * mh = (struct mach_header_64 *) ki->address;
+    struct segment_command_64 * seg =
+        (struct segment_command_64 *) ((uint64_t) mh + sizeof(struct mach_header_64));
+
+    void * end = (void *) ((uint64_t)mh + mh->sizeofcmds);
+
+    while (seg < end) {
+        if (seg->cmd == LC_SEGMENT_64) {
+            if (!strcmp(seg->segname, "__DATA")) {
+                struct section_64 * sect =
+                    (struct section_64 *) ((uint64_t)seg + sizeof(struct segment_command_64));
+                while (sect < (uint64_t)seg + seg->cmdsize) {
+                    if (!strcmp(sect->sectname, "__mod_init_func")) {
+                        structor_t *ctor = (structor_t *) sect->addr;
+                        while (ctor < (uint64_t)sect->addr + sect->size) {
+                            if (*ctor) {
+                                kprintf("calling constructor %p\n", *ctor);
+                                (**ctor)();
+                            }
+                            ctor++;
+                        }
+                        return;
+                    }
+                    sect = (struct section_64 *) ((uint64_t)sect + sizeof(struct section_64));
+                }
+            }
+        }
+        seg = (struct segment_command_64 *) ((uint64_t)seg + seg->cmdsize);
+    }
+}
+
 /*********************************************************************
 *********************************************************************/
 __private_extern__ kern_return_t
 _start(kmod_info_t *ki, void *data)
 {
+        __call_constructors(ki);
+        
 	if (_realmain) {
 		return (*_realmain)(ki, data);
 	} else {
